4c8a51e9d479562d4eaff7c7965eee5cf14b1cf2
Mary Beth Hunter <marybeth.hunter@okta.com>
2023-09-22 11:41:42 -0500

    update

--- src/components/Metadata.js
+++ src/components/Metadata.js
@@ -9,9 +9,10 @@ const providerConfig = {
 };
 
 const Metadata = () => {
-  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const { user, isAuthenticated, getAccessTokenSilently, getIdTokenClaims } = useAuth0();
   const [userMetadata, setUserMetadata] = useState(null);
   const [userAccessToken, setUserAccessToken] = useState(null);
+  const [userIdToken, setUserIdToken] = useState(null);
 
   useEffect(() => {
     const getUserMetadata = async () => {
@@ -35,8 +36,13 @@ const Metadata = () => {
 
         const { user_metadata } = await metadataResponse.json();
 
+
+        const claims = await getIdTokenClaims();
+        const id_token = claims.__raw;
+
         setUserMetadata(user_metadata);
         setUserAccessToken(accessToken);
+        setUserIdToken(id_token);
       } catch (e) {
         console.log(e.message);
       }
@@ -81,6 +87,25 @@ const Metadata = () => {
             </pre>
           ) : (
             <p style={{ fontSize: "13px" }}>No user access token defined</p>
+          )}
+        </div>
+      </div>
+      <div style={{ width: "550px", overflowX: "scroll" }}>
+        <div
+          style={{
+            border: "1px solid white",
+            padding: "12px",
+            marginTop: 22,
+            width: "fit-content",
+            backgroundColor: "#00A36C",
+          }}
+        >
+          {userIdToken ? (
+            <pre style={{ fontSize: "13px" }}>
+              ID Token: <code>{userIdToken}</code>
+            </pre>
+          ) : (
+            <p style={{ fontSize: "13px" }}>No user ID token defined</p>
           )}
         </div>
       </div>
