09a728d0d94bc9f231382a3d1ce7a2d7bc47f20e
Mary Beth Hunter <marybeth.hunter@okta.com>
2023-09-22 12:08:25 -0500

    refactor env variables

--- src/config.js
+++ src/config.js
@@ -1,11 +1,9 @@
-import configJson from "./auth_config.json";
-
 export function getConfig() {
   return {
-    domain: configJson.domain || process.env.REACT_APP_AUTH0_DOMAIN,
-    clientId: configJson.clientId || process.env.CLIENTID,
-    audience: configJson.audience || process.env.AUDIENCE,
-    scope: configJson.scope || process.env.SCOPE,
-    callbackURL: configJson.callbackURL || process.env._URL,
+    domain: process.env.REACT_APP_AUTH0_DOMAIN,
+    clientId: process.env.REACT_APP_AUTH0_CLIENTID,
+    audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+    scope: process.env.REACT_APP_SCOPE,
+    callbackURL: process.env.REACT_APP_CALLBACK_URL,
   };
 }
