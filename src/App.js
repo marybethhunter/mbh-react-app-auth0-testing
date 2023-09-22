15399a3e6a416f649083adec96db8c1501fda5ec
Mary Beth Hunter <marybeth.hunter@okta.com>
2023-09-21 19:38:29 -0500

    login, logout, config

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  };
+
+  console.log(user);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          MB's Custom React App :)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div style={{ display: 'flex', flexDirection: 'row', gap: 8, marginBottom: 20 }}>
+          <LoginButton />
+          <LogoutButton />
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'row', gap: 4}}>
+          <p>User is Authenticated:</p>
+          {isAuthenticated ? (<p>True</p>) : (<p>False</p>)}
+        </div>
+        {isAuthenticated && (
+          <div>
+            <h3>User Profile Info:</h3>
+            <p>User Name: {user.name}</p>
+            <p>User Email: {user.email}</p>
+          </div>
+        )}
+      </header >
+    </div >
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { getConfig } from "./config";
+
+const config = getConfig();
+
+const providerConfig = {
+  domain: config.domain,
+  clientId: config.clientId,
+  authorizationParams: {
+    redirect_uri: window.location.origin,
+    ...(config.audience ? { audience: config.audience } : null),
+  },
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      {...providerConfig}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
